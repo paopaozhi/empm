b2f92e4e4d1831528a80545ce9908034b0011103
paopaozhi <paopaozhi@hotmail.com>
2024-05-20 11:49:07 +0800

    feat:更换icon，添加已安装包列表展示

--- web/html/src/pages/Home.tsx
+++ web/html/src/pages/Home.tsx
@@ -1,22 +1,72 @@
-import { Button, Col, Layout, List, Row } from "@douyinfe/semi-ui";
+import {
+  Button,
+  ButtonGroup,
+  Col,
+  Descriptions,
+  Layout,
+  List,
+  Row,
+} from "@douyinfe/semi-ui";
 import { IconGithubLogo, IconMoon, IconSun } from "@douyinfe/semi-icons";
 import { useState } from "react";
+import "./Home.css";
 
-const ListShow = () => {
-  const data = ["名称", "版本", "作者"];
+function ListShow({
+  titleName,
+  data,
+  renderType,
+}: {
+  titleName: string;
+  data: any;
+  renderType: boolean;
+}) {
+  const ListRenderType = (item: any) => {
+    if (renderType) {
+      return (
+        <Row>
+          <Col span={12}>
+            <div className="col-list">{item[0]}</div>
+          </Col>
+          <Col span={12}>
+            <div className="col-list">{item[1]}</div>
+          </Col>
+        </Row>
+      );
+    } else {
+      return (
+        <List.Item
+          main={
+            <div>
+              <Descriptions
+                align="center"
+                size="small"
+                row
+                data={[
+                  { key: "名称", value: item.name },
+                  { key: "版本", value: item.version },
+                  { key: "链接", value: item.url },
+                ]}
+              />
+            </div>
+          }
+          extra={
+            <ButtonGroup theme="borderless">
+              <Button>修改</Button>
+              <Button>删除</Button>
+            </ButtonGroup>
+          }
+        />
+      );
+    }
+  };
 
   return (
-    <>
-      <div>
-        <h3 className="titleStyle">基本信息</h3>
-        <List
-          dataSource={data}
-          renderItem={(item) => <List.Item>{item}</List.Item>}
-        />
-      </div>
-    </>
+    <div>
+      <h3 className="titleStyle">{titleName}</h3>
+      <List dataSource={data} renderItem={ListRenderType} />
+    </div>
   );
-};
+}
 
 function Home() {
   const [switchIcon, setswitchIcon] = useState(<IconMoon />);
@@ -39,6 +89,17 @@ function Home() {
   };
 
   const { Header, Content } = Layout;
+
+  const myMap = [
+    ["名称", "模板工程"],
+    ["版本", "0.0.7"],
+    ["作者", "paopaozhi"],
+  ];
+
+  const pack_info = [
+    { name: "ulog", version: "0.0.1", url: "https://github.com/rdpoor/ulog" },
+    { name: "ulog", version: "0.0.1", url: "https://github.com/rdpoor/ulog" },
+  ];
 
   return (
     <>
@@ -63,8 +124,12 @@ function Home() {
           </Row>
         </Header>
         <Content style={{ height: 300 }}>
-          <ListShow />
-          {/* <Image width={360} height={200} src={demoPng} /> */}
+          <ListShow titleName="基本信息" data={myMap} renderType />
+          <ListShow
+            titleName="已安装的包"
+            data={pack_info}
+            renderType={false}
+          />
         </Content>
         {/* <Footer style={commonStyle}>Footer</Footer> */}
       </Layout>
