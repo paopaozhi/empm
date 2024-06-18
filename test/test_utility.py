d7611ab500e84c56e5d6ceba22ba85379b150d82
paopaozhi <paopaozhi@hotmail.com>
2024-06-18 16:20:10 +0800

    test:重构test使其依赖pytest，将测试环境构建抽象到utility

--- test/test_utility.py
+++ test/test_utility.py
@@ -1,55 +1,22 @@
 import os
-import shutil
-import subprocess
 import unittest
 from pathlib import Path
 
-from empm.utility import download_release, download_repo
 
-from .utility.env_manage import auto_clear_env
+def test_get_depend():
+    pass
 
 
-class TestUtility(unittest.TestCase):
-    def test_downloadRepo(self):
-        # clean env, delete file
-        lib_path = Path("lib/ulog")
+def test_set_depend():
+    pass
 
-        if lib_path.exists():
-            if os.name == "nt":
-                subprocess.run(
-                    ["rmdir", "/S", "/Q", lib_path],
-                    shell=True,
-                )
-            elif os.name == "posix":
-                shutil.rmtree(lib_path)
 
-        download_repo("https://github.com/rdpoor/ulog", "ulog")
+def test_delete_pack():
+    from empm.utility import delete_pack
 
-        ret = os.path.isdir(lib_path)
-        self.assertEqual(True, ret)
+    pack_path = Path("lib/ulogs")
 
-    @auto_clear_env
-    def test_downloadRelease(self):
-        lib_path = Path("lib/gitmoji")
-        download_release(
-            "https://github.com/carloscuesta/gitmoji", name="gitmoji", version="v3.14.0"
-        )
+    os.makedirs("lib/ulogs", exist_ok=True)
+    delete_pack(pack_path)
 
-        ret = os.path.isdir(lib_path)
-        assert ret is True
-
-    def test_get_depend(slef):
-        pass
-
-    def test_set_depend(self):
-        pass
-
-    def test_delete_pack(self):
-        from empm.utility import delete_pack
-
-        pack_path = Path("lib/ulogs")
-
-        os.makedirs("lib/ulogs", exist_ok=True)
-        delete_pack(pack_path)
-
-        assert Path("lib/ulogs").exists() is False
+    assert Path("lib/ulogs").exists() is False
--- test/test_web.py
+++ test/test_web.py
@@ -1,14 +1,16 @@
-import toml
 from fastapi.testclient import TestClient
 
+from empm._internal.web.main import web_app
 from test.utility import env_manage
-from web.main import web_app
+
+from .utility import write_test_toml
 
 client = TestClient(web_app)
 
+
 @env_manage.auto_clear_env
 def test_read_main():
-    empm_cfg = """
+    testConfig = """
     [package]
     name = "empm"
     version = "0.1.0"
@@ -19,12 +21,7 @@ def test_read_main():
     ulog = { url = "https://github.com/rdpoor/ulog" }
     """
 
-    cfg_class = {}
-    cfg_class = toml.loads(empm_cfg)
-
-    with open("empm.toml", "w") as fd:
-        if cfg_class:
-            toml.dump(cfg_class, fd)
+    write_test_toml(testConfig)
 
     response = client.get("/")
     assert response.status_code == 200
