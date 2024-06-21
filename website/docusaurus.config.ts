1c9df500cc6aaafd460777a49d9e1c976f387e50
paopaozhi <paopaozhi@hotmail.com>
2024-06-21 19:54:49 +0800

    ci:添加website部署

--- website/docusaurus.config.ts
+++ website/docusaurus.config.ts
@@ -8,7 +8,7 @@ const config: Config = {
   favicon: 'img/empm-icon.png',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://empm.paopaozhi.ltd',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
@@ -17,6 +17,8 @@ const config: Config = {
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'paopaozhi', // Usually your GitHub org/user name.
   projectName: 'empm', // Usually your repo name.
+  deploymentBranch: 'gh-pages',
+  trailingSlash: false,
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
